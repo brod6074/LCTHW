85f451e0ac032a4bd65ec1b222ffcd524e196b29
Roberto Rodriguez <brod6074@users.noreply.github.com>
2015-08-06 18:39:35 -0700

    Made small changes

--- ex19/object.h
+++ ex19/object.h
@@ -5,20 +5,22 @@ typedef enum {
     NORTH, SOUTH, EAST, WEST
 } Direction;
 
-typedef struct {
+struct Object {
     char* description;
     int (*init)(void* self);
     void (*describe)(void* self);
     void (*destroy)(void* self);
     void* (*move)(void* self, Direction direction);
     int (*attack)(void* self, int damage);
-} Object;
+};
+
+typedef struct Object Object;
 
 int Object_init(void* self);
 void Object_destroy(void* self);
 void Object_describe(void* self);
 void* Object_move(void* self, Direction direction);
-int Object_attack(void*self, int damage);
+int Object_attack(void* self, int damage);
 void* Object_new(size_t size, Object proto, char* description);
 
 #define NEW(T, N) Object_new(sizeof(T), T##Proto, N)
